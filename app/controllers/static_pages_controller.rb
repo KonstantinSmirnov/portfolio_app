b373f5f28ef86dbbf972768b1280ce472539ba17
konstantinsmirnov <rusksmi@gmail.com>
2016-02-10 00:53:24 +0300

    Add static pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,12 @@
 class StaticPagesController < ApplicationController
 
   def index
+  end
 
+  def projects
+  end
+
+  def contacts
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
+  get 'projects' => 'static_pages#projects'
+  get 'contacts' => 'static_pages#contacts'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
