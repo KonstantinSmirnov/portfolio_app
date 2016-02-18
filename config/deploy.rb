4a50d1f987852af9ccf5b3c074db9d4b3f673bf6
konstantinsmirnov <rusksmi@gmail.com>
2016-02-18 18:41:24 +0300

    Replace github repo link

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.1.0'
 
 set :application, 'portfolio_app'
-set :repo_url, 'git@github.com:KonstantinSmirnov/portfolio_app.git'
+set :repo_url, 'https://github.com/KonstantinSmirnov/portfolio_app.git'
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
