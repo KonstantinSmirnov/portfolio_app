566755130a6db44c9079c5938ce5a81b5a275481
konstantinsmirnov <rusksmi@gmail.com>
2016-02-22 12:08:45 +0300

    Add image default size and relocate default image

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,7 +3,9 @@ class Project < ActiveRecord::Base
   validates :description, presence: true
   validates :link, presence: true
 
-  has_attached_file :image
+  has_attached_file :image, :styles => { :normal => "600x" },
+                  :default_style => :normal,
+                  :default_url => 'missing_image.png'
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 
 end
