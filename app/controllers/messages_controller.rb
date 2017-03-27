17524893d0968fbdaba7ca170fdc7e10d2d047ea
Konstantin <rusksmi@gmail.com>
2017-03-27 11:05:03 +0000

    Update mailer

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,9 +4,14 @@ class MessagesController < ApplicationController
     @message = Message.new(message_params)
 
     if @message.valid?
-      MessageMailer.new_message(admin_email, @message, @content.logo_text).deliver
-      flash[:success] = "Your messages has been sent."
-      redirect_to contact_path
+      if MessageMailer.new_message(admin_email, @message, @content.logo_text).deliver
+        flash[:success] = "Your messages has been sent."
+        redirect_to contact_path
+      else
+        flash[:danger] = "An error occurred while delivering this message."
+        #redirect_to contact_path
+        render 'static_pages/contact'
+      end
     else
       flash[:danger] = "An error occurred while delivering this message."
       #redirect_to contact_path
--- config/application.rb
+++ config/application.rb
@@ -35,11 +35,11 @@ module PortfolioApp
 
     # set up mailer server
     config.action_mailer.smtp_settings = {
-      :address              => ENV["SMTP_ADDRESS"],
-      :port                 => ENV["SMTP_PORT"],
-      :domain               => ENV["SMTP_DOMAIN"],
+      :address              => 'smtp.gmail.com',
+      :port                 => 587,
+      :domain               => 'gmail.com',
       :user_name            => ENV['SMTP_USER_NAME'],
-      :password             => ENV["SMTP_PASSWORD"],
+      :password             => ENV['SMTP_PASSWORD'],
       :authentication       => :plain,
       :enable_starttls_auto => true
     }
